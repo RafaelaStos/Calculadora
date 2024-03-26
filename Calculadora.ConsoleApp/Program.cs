c2a25c6644e9f37a4fdeb7719617a587b996dbe7
Rafaela dos Santos Morais <morais.rafa508@gmail.com>
2024-03-25 23:06:53 -0300

    Primeira Refatoraração

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -6,71 +6,105 @@
         {
             while (true)
             {
-                Console.Clear();
+                string opcao = MostrarMenu();
 
-                Console.WriteLine("Calculadora 2024");
-                Console.WriteLine("");
+                if (OpcaoSaidaSelecionada(opcao))
+                    break;
 
-                Console.WriteLine("Digite 1 para Adição");
-                Console.WriteLine("Digite 2 para Subtrair");
-                Console.WriteLine("Digite 3 para multiplição");
-                Console.WriteLine("Digite 4 para Divisão");
-                Console.WriteLine("Digite S para sair");
 
-                string operacao = Console.ReadLine();
-                Console.WriteLine("");
-
-                if (operacao == "1" || operacao == "2" || operacao == "3" || operacao == "4" ||
-                    operacao == "S" || operacao == "s")
+                else if (OpcaoInvalida(opcao))
                 {
-                    if (operacao == "S" || operacao == "s")
-                        break;
-
-                    Console.WriteLine("Digite o primeiro Número: ");
-                    string primeiroNumeroString = "";
-                    primeiroNumeroString = Console.ReadLine();
-                    decimal primeiroNumero = Convert.ToDecimal(primeiroNumeroString);
-
-                    Console.WriteLine("Digite o segundo Número: ");
-                    string segundoNumeroString = "";
-                    segundoNumeroString = Console.ReadLine();
-                    decimal segundoNumero = Convert.ToDecimal(segundoNumeroString);
-
-                    decimal resultado = 0;
-
-                    if (operacao == "1")
-
-                        resultado = primeiroNumero + segundoNumero;
-
-                    else if (operacao == "2")
-
-                        resultado = primeiroNumero - segundoNumero;
-
-                    else if (operacao == "3")
-
-                        resultado = primeiroNumero * segundoNumero;
-
-                    else if (operacao == "4")
-                    {
-
-                        while (segundoNumero == 0)
-                        {
-                            Console.WriteLine("Não é possivel faze divizão por zero ");
-                            Console.WriteLine("Digite o segundo Número: ");
-                            segundoNumeroString = Console.ReadLine();
-                            segundoNumero = Convert.ToDecimal(segundoNumeroString);
-                        }
-                        resultado = primeiroNumero / segundoNumero;
-                    }
-                    Console.WriteLine("");
-                    Console.WriteLine("O resultado é: " + resultado);
-
+                    ExibirMenssagemErro();
+                    continue;
                 }
                 else
-                    Console.WriteLine("Opção invalida. Tente novamente!");
-                Console.ReadLine();
+                    RealizarCalculo(opcao);
+
+                
+            }
+        }
+
+        static string MostrarMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Calculadora 2024");
+            Console.WriteLine("");
+
+            Console.WriteLine("Digite 1 para Adição");
+            Console.WriteLine("Digite 2 para Subtrair");
+            Console.WriteLine("Digite 3 para multiplição");
+            Console.WriteLine("Digite 4 para Divisão");
+            Console.WriteLine("Digite S para sair");
+
+            string operacao = Console.ReadLine();
+            Console.WriteLine("");
+
+            return operacao;
+        }
+
+        static bool OpcaoSaidaSelecionada(string opcao)
+        {
+            bool opcaoSeecionada = opcao == "S" || opcao == "s";
+
+            return opcaoSeecionada;
+        }
+
+        static bool OpcaoInvalida(string opcao)
+        {
+            bool opcaoInvalida = opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4" &&
+                    opcao != "S" && opcao != "s";
+
+            return opcaoInvalida;
+        }
+
+        private static void ExibirMenssagemErro()
+        {
+            Console.WriteLine("Opção invalida. Tente novamente!");
+            Console.ReadLine();
+        }
+        static void RealizarCalculo(string opcao)
+        {
+
+            Console.WriteLine("Digite o primeiro Número: ");
+            decimal primeiroNumero = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo Número: ");
+            decimal segundoNumero = Convert.ToDecimal(Console.ReadLine());
+
+            decimal resultado = 0;
+            switch (opcao)
+
+            {
+                case "1":
+                    resultado = primeiroNumero + segundoNumero;
+                    break;
+                case "2":
+                    resultado = primeiroNumero - segundoNumero;
+                    break;
+                case "3":
+                    resultado = primeiroNumero * segundoNumero;
+                    break;
+                case "4":
+                    while (segundoNumero == 0)
+                    {
+                        Console.WriteLine("Não é possivel faze divizão por zero ");
+                        Console.WriteLine("Digite o segundo Número: ");
+                        segundoNumero = Convert.ToDecimal(Console.ReadLine());
+                    }
+                    resultado = primeiroNumero / segundoNumero;
+                    break;
 
             }
+
+            ExibirResultado(resultado);
+
+        }
+
+        private static void ExibirResultado(decimal resultado)
+        {
+            Console.WriteLine("O resultado é: " + resultado);
+            Console.ReadLine();
         }
     }
 }
