6a9ba70e9bf8390f4e5c51c43cde5a7521c3ae9e
Rafaela dos Santos Morais <morais.rafa508@gmail.com>
2024-03-26 00:35:50 -0300

    Opção Tabuada e historico de operação

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -2,8 +2,14 @@
 {
     internal class Program
     {
+        static int operacaoRealizadas = 0;
         static void Main(string[] args)
+        
         {
+
+            string[] historicoOperacoes = new string[100];
+
+            
             while (true)
             {
                 string opcao = MostrarMenu();
@@ -17,12 +23,23 @@
                     ExibirMenssagemErro();
                     continue;
                 }
-                else
-                    RealizarCalculo(opcao);
-
+                else if (opcao == "5")
+                {
+                    GerarTabuada();
+                }
+                else if (opcao == "6")
+                {
+                    VisualizarHIstoricoOperacoes(historicoOperacoes);
                 
+                }
+                else
+                    RealizarCalculo(opcao, historicoOperacoes);
+
+
             }
         }
+
+
 
         static string MostrarMenu()
         {
@@ -33,9 +50,11 @@
 
             Console.WriteLine("Digite 1 para Adição");
             Console.WriteLine("Digite 2 para Subtrair");
-            Console.WriteLine("Digite 3 para multiplição");
+            Console.WriteLine("Digite 3 para Multiplição");
             Console.WriteLine("Digite 4 para Divisão");
-            Console.WriteLine("Digite S para sair");
+            Console.WriteLine("Digite 5 para Gerar a Tabuada");
+            Console.WriteLine("Digite 6 para Historico de Operações");
+            Console.WriteLine("Digite S para Sair");
 
             string operacao = Console.ReadLine();
             Console.WriteLine("");
@@ -52,8 +71,15 @@
 
         static bool OpcaoInvalida(string opcao)
         {
-            bool opcaoInvalida = opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4" &&
-                    opcao != "S" && opcao != "s";
+            bool opcaoInvalida =
+                opcao != "1" &&
+                opcao != "2" &&
+                opcao != "3" &&
+                opcao != "4" &&
+                opcao != "5" &&
+                opcao != "6" &&
+                opcao != "S" &&
+                opcao != "s";
 
             return opcaoInvalida;
         }
@@ -63,7 +89,8 @@
             Console.WriteLine("Opção invalida. Tente novamente!");
             Console.ReadLine();
         }
-        static void RealizarCalculo(string opcao)
+
+        static void RealizarCalculo(string opcao, string[] historicoOperacoes)
         {
 
             Console.WriteLine("Digite o primeiro Número: ");
@@ -73,32 +100,47 @@
             decimal segundoNumero = Convert.ToDecimal(Console.ReadLine());
 
             decimal resultado = 0;
+            string operador = "";
             switch (opcao)
 
             {
                 case "1":
                     resultado = primeiroNumero + segundoNumero;
+                    operador = "+";
                     break;
                 case "2":
                     resultado = primeiroNumero - segundoNumero;
+                    operador = "-";
                     break;
                 case "3":
                     resultado = primeiroNumero * segundoNumero;
+                    operador = "x";
                     break;
                 case "4":
-                    while (segundoNumero == 0)
-                    {
-                        Console.WriteLine("Não é possivel faze divizão por zero ");
-                        Console.WriteLine("Digite o segundo Número: ");
-                        segundoNumero = Convert.ToDecimal(Console.ReadLine());
-                    }
-                    resultado = primeiroNumero / segundoNumero;
+                    resultado = Dividir(primeiroNumero, ref segundoNumero);
+                    operador = "/";
                     break;
+
 
             }
 
+            historicoOperacoes[operacaoRealizadas] = $"{primeiroNumero} {operador} {segundoNumero} = {resultado}";
+            operacaoRealizadas++;
             ExibirResultado(resultado);
 
+        }
+
+        private static decimal Dividir(decimal primeiroNumero, ref decimal segundoNumero)
+        {
+            decimal resultado;
+            while (segundoNumero == 0)
+            {
+                Console.WriteLine("Não é possivel faze divizão por zero ");
+                Console.WriteLine("Digite o segundo Número: ");
+                segundoNumero = Convert.ToDecimal(Console.ReadLine());
+            }
+            resultado = primeiroNumero / segundoNumero;
+            return resultado;
         }
 
         private static void ExibirResultado(decimal resultado)
@@ -106,5 +148,37 @@
             Console.WriteLine("O resultado é: " + resultado);
             Console.ReadLine();
         }
+        private static void GerarTabuada()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite um numero inteito;");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Tabuado do numero: {numeroDigitado}");
+            Console.WriteLine();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{numeroDigitado} x {i} = {numeroDigitado * i}");
+            }
+            Console.ReadLine();
+        }
+
+         static void VisualizarHIstoricoOperacoes(string[] historicoOperacoes)
+        {
+            Console.WriteLine("Exibir Historico de Operações");
+            Console.WriteLine("-------------------------------");
+
+            for (int operacao = 0; operacao < historicoOperacoes.Length; operacao++)
+            {
+                if (historicoOperacoes[operacao] != null)
+                Console.WriteLine(historicoOperacoes[operacao]);
+            }
+
+
+            Console.WriteLine("Digite ENTER para continuar");
+
+            Console.WriteLine();
+        }
+
     }
 }
